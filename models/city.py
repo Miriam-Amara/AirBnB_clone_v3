37f8cc733fe50f1569d6e7a9cfe682b021e19494
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-08-24 05:47:25 +0100

    chore(models/city): integrate SQLAlchemy ORM

--- models/city.py
+++ models/city.py
@@ -4,13 +4,19 @@
 This module contains City class for the HBNB system.
 """
 
-from models.basemodel import BaseModel
+from sqlalchemy.orm import mapped_column, relationship
+from sqlalchemy import String, ForeignKey
+
+from models.basemodel import BaseModel, Base
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """
     Defines City attributes and methods for the system.
     """
+    __tablename__ = "cities"
 
-    state_id: str = ""
-    name: str = ""
+    name = mapped_column(String(128), nullable=False)
+    state_id = mapped_column(String(60), ForeignKey("states.id"), nullable=False)
+    state = relationship("State", back_populates="cities")
+    places = relationship("Place", backref="cities", cascade="all, delete-orphan")
