150cf8cb7e7031ed0a8aec570488013a5a695287
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-08-24 05:48:55 +0100

    chore(models/user): integrate SQLAlchemy ORM

--- models/user.py
+++ models/user.py
@@ -4,15 +4,21 @@
 This module contains User class for the HBNB system.
 """
 
-from models.basemodel import BaseModel
+from sqlalchemy import String
+from sqlalchemy.orm import mapped_column, relationship
+
+from models.basemodel import BaseModel, Base
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """
     Defines blueprint for users in the system.
     """
+    __tablename__ = "users"
 
-    email: str = ""
-    password: str = ""
-    first_name: str = ""
-    last_name: str = ""
+    email = mapped_column(String(128), nullable=False)
+    password = mapped_column(String(128), nullable=False)
+    first_name = mapped_column(String(128), nullable=False)
+    last_name = mapped_column(String(128), nullable=False)
+    places = relationship("Place", backref="user", cascade="all, delete-orphan")
+    reviews = relationship("Review", backref="user", cascade="all, delete-orphan")
