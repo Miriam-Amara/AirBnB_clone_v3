9ccbd34aec63a7d8fd7e6423c8d3a33c5266db74
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-08-24 05:48:23 +0100

    chore(models/state): integrate SQLAlchemy ORM

--- models/state.py
+++ models/state.py
@@ -4,12 +4,16 @@
 This module contains State class for the HBNB system.
 """
 
-from models.basemodel import BaseModel
+from sqlalchemy.orm import mapped_column, relationship
+from sqlalchemy import String
+
+from models.basemodel import BaseModel, Base
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """
     Defines the State attributes and methods for the system.
     """
-
-    name: str = ""
+    __tablename__ = "states"
+    name = mapped_column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state", cascade="all, delete-orphan")
