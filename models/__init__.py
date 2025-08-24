a3fdc516443053fd1c1fe4bb5e7c2ba1acfbf958
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-08-24 05:41:12 +0100

    feat(models): support dynamic storage backend in __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
-from models.engine.file_storage import FileStorage
+from dotenv import load_dotenv
+import os
 
-storage = FileStorage()
-storage.reload()
+load_dotenv()
+
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+elif os.getenv("HBNB_TYPE_STORAGE") == "file":
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
