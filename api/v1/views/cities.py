87687e8a69fede563bcdd60128f54688a0aee2dd
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-08-31 21:13:36 +0100

    feat(api/v1/views/cities): add CRUD endpoints for cities

---
+++ api/v1/views/cities.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+"""
+
+"""
+
+from flask import abort, jsonify
+from typing import Any
+import logging
+
+from api.v1.views import app_views
+from api.v1.views.utils import get_obj, get_request_data
+from models import storage
+from models.city import City
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(filename)s - %(message)s",
+    filename="api/v1/views/views.log",
+    force=True
+)
+
+disable_logging: bool = False
+if disable_logging:
+    logging.disable(logging.CRITICAL)
+
+
+@app_views.route("/states/<state_id>/cities", methods=["POST"])
+def create_city(state_id: str):
+    """Creates a city and saves it to database."""
+    state_obj = get_obj("State", state_id)
+    city_data = get_request_data()
+    
+    if "name" not in city_data:
+        abort(400, description="Missing name")
+
+    city_data["state_id"] = state_obj.id
+    try:
+        city = City(**city_data)
+        city.save()
+    except Exception as e:
+        logging.error(f"{e}")
+        abort(500, description="Unsuccessful")
+    return city.to_dict(), 201
+
+@app_views.route("/states/<state_id>/cities")
+def get_state_cities(state_id: str):
+    """Retrieves all the cities in a state."""
+    state_obj = get_obj("State", state_id)
+
+    state_cities: list[dict[str, Any]] = [city.to_dict() for city in state_obj.cities] # type: ignore
+    return state_cities, 200
+
+@app_views.route("/cities/<city_id>", methods=["PUT"])
+def update_city(city_id: str):
+    """Updates a city in the database"""
+    city_obj = get_obj("City", city_id)
+    city_data = get_request_data()
+
+    city_data.pop("id", None)
+    city_data.pop("state_id", None)
+    city_data.pop("created_at", None)
+    city_data.pop("updated_at", None)
+
+    for attr, value in city_data.items():
+        setattr(city_obj, attr, value)
+    city_obj.save()
+    return city_obj.to_dict(), 200
+    
+@app_views.route("/cities/<city_id>", methods=["DELETE"])
+def delete_city(city_id: str):
+    """Deletes a city from the database."""
+    city_obj = get_obj("City", city_id)
+    storage.delete(city_obj)
+    storage.save()
+    return jsonify({}), 200
+
