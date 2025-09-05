a238630f43134f6566264ea0bde3d6d50c2f2d5d
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-09-05 20:20:46 +0100

    refactor(api/v1/views/utils): add type annotations

--- api/v1/views/utils.py
+++ api/v1/views/utils.py
@@ -5,17 +5,19 @@
 """
 
 from flask import abort, request
+from typing import Any, Optional
 
 from models import storage
+from models.basemodel import BaseModel
 
 def get_obj(cls: str, id: str):
     """Checks whether an object exists in the database using its class and id."""
-    obj = storage.get(cls, id)
+    obj: Optional[BaseModel] = storage.get(cls, id)
     if not obj:
         abort(404, description=f"{cls} {id} not found")
     return obj
 
-def get_request_data():
+def get_request_data() -> dict[str, Any]:
     """Checks if a request data is valid json."""
     try:
         obj_data = request.get_json()
