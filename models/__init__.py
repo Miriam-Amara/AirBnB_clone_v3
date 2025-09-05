6795df1cd18286ac6dd97139a3fd6665c2cec1aa
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-09-05 01:16:07 +0100

    chore(models): comment out FileStorage import and initialization

--- models/__init__.py
+++ models/__init__.py
@@ -9,7 +9,7 @@ if os.getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
-elif os.getenv("HBNB_TYPE_STORAGE") == "file":
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
-    storage.reload()
+# elif os.getenv("HBNB_TYPE_STORAGE") == "file":
+#     from models.engine.file_storage import FileStorage
+#     storage = FileStorage()
+#     storage.reload()
