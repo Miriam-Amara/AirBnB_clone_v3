f02711d75dbc4135c2474a2aa229aae691692b68
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-09-05 20:13:38 +0100

    refactor(api/v1/views/places_amenities): add strict_slashes to all routes

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-@app_views.route("/places/<place_id>/amenities/<amenity_id>", methods=["POST"])
+@app_views.route("/places/<place_id>/amenities/<amenity_id>", strict_slashes=False, methods=["POST"])
 def add_place_amenity(place_id: str, amenity_id: str):
     """Links amenity object to a place"""
     place_obj = cast(Place, get_obj("Place", place_id))
@@ -30,14 +30,14 @@ def add_place_amenity(place_id: str, amenity_id: str):
     place_obj.save()
     return amenity_obj.to_dict(), 201
 
-@app_views.route("/places/<place_id>/amenities")
+@app_views.route("/places/<place_id>/amenities", strict_slashes=False)
 def get_place_amenities(place_id: str):
     """Retrieves the list of all amenities of a Place."""
     place_obj = cast(Place, get_obj("Place", place_id))
     amenities: list[dict[str, Any]] = [amenity.to_dict() for amenity in place_obj.amenities]
     return amenities, 200
 
-@app_views.route("/places/<place_id>/amenities/<amenity_id>", methods=["DELETE"])
+@app_views.route("/places/<place_id>/amenities/<amenity_id>", strict_slashes=False, methods=["DELETE"])
 def delete_place_amenity(place_id: str, amenity_id: str):
     """Deletes an Amenity object to a Place"""
     place_obj = cast(Place, get_obj("Place", place_id))
