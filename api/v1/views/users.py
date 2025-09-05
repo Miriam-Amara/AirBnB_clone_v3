d703332104a9c9c4a70d421dadf92accdb8087e7
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-09-05 20:19:06 +0100

    feat(api/v1/views/users): add email uniqueness check and bcrypt password hashing in /register

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -13,53 +13,53 @@ from models import storage
 from models.user import User
 
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s - %(levelname)s - %(filename)s - %(message)s",
-    filename="api/v1/views/views.log",
-    force=True
-)
-
-disable_logging: bool = False
-if disable_logging:
-    logging.disable(logging.CRITICAL)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
-
-@app_views.route("/users", methods=["POST"])
+@app_views.route("/register", strict_slashes=False, methods=["POST"])
 def create_user():
     """Adds a user to the database."""
+    from api.v1.app import bycrypt
     user_data = get_request_data()
 
     if "email" not in user_data:
         abort(400, description="Missing email")
+    if User.search(user_data["email"]):
+        abort(400, description="Email already exists")
     if "password" not in user_data:
         abort(400, description="Missing password")
     if "first_name" not in user_data:
-        abort(400, description="Missing first name")
+        abort(400, description="Missing first_name")
     if "last_name" not in user_data:
-        abort(400, description="Missing last name")
+        abort(400, description="Missing last_name")
     
+    hashed_password: str = (
+        bycrypt
+        .generate_password_hash(user_data["password"]) # type: ignore
+        .decode('utf-8')
+    )
+    user_data["password"] = hashed_password
     try:
         user = User(**user_data)
         user.save()
     except Exception as e:
-        logging.error(f"{e}")
+        logger.error(f"{e}")
         abort(500, description="Unsuccessful")
     return user.to_dict(), 201
 
-@app_views.route("/users")
+@app_views.route("/users", strict_slashes=False)
 def all_user():
     """Retrieves a list of all users in the database."""
     return storage.all("User"), 200
 
-@app_views.route("/users/<user_id>")
+@app_views.route("/users/<user_id>", strict_slashes=False)
 def get_user(user_id: str):
     """Retrieves a user from the database"""
     user_obj = get_obj("User", user_id)
     return user_obj.to_dict(), 200
 
-@app_views.route("/users/<user_id>", methods=["PUT"])
+@app_views.route("/users/<user_id>", strict_slashes=False, methods=["PUT"])
 def update_user(user_id: str):
     """Updates a user info in the database"""
     user_obj = get_obj("User", user_id)
@@ -69,13 +69,14 @@ def update_user(user_id: str):
     user_data.pop("created_at", None)
     user_data.pop("updated_at", None)
     user_data.pop("email", None)
+    user_data.pop("password", None)
 
     for attr, value in user_data.items():
         setattr(user_obj, attr, value)
     user_obj.save()
     return user_obj.to_dict(), 200
 
-@app_views.route("/users/<user_id>", methods=["DELETE"])
+@app_views.route("/users/<user_id>", strict_slashes=False, methods=["DELETE"])
 def delete_user(user_id: str):
     """Deletes a user from database."""
     user_obj = get_obj("User", user_id)
