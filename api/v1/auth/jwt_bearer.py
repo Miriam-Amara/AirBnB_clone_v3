00560f84bf81c285fae6f8798699f9d79f8b54f6
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-09-05 19:15:43 +0100

    refactor(api/v1/auth/jwt_bearer): remove reset token logic

--- api/v1/auth/jwt_bearer.py
+++ api/v1/auth/jwt_bearer.py
@@ -32,14 +32,12 @@ class JWTBearerAuth(Auth):
         """
         Extracts and returns the JWT token from a Bearer Authorization header.
         """
-        logger.debug(f"{authorization_header}")
         if authorization_header is None:
             return
         if not isinstance(authorization_header, str): # type: ignore
             return
         if not authorization_header.startswith("Bearer "):
             return
-        logger.debug(f"{authorization_header.split()[1]}")
         return authorization_header.split()[1]
     
     def decode_jwt_token(
@@ -55,34 +53,23 @@ class JWTBearerAuth(Auth):
             decoded_data: dict[str, Any] = jwt.decode( # type: ignore
                 token, os.getenv("SECRET_KEY"), algorithms=["HS256"]
             )
-            logger.debug(f"{decoded_data}")
             return decoded_data
-        except jwt.ExpiredSignatureError:
-            pass
-        except jwt.InvalidTokenError as e:
+        except Exception as e:
             logger.debug(f"{e}")
             return
     
-    def encode_jwt_token(self, user_id: str) -> tuple[str, str]:
+    def encode_jwt_token(self, user_id: str) -> str:
         """
         
         """
         access_token_data: dict[str, Any] = {
             "sub": user_id,
-            "exp": datetime.now() + timedelta(minutes=30)
+            "exp": datetime.now() + timedelta(minutes=15)
         }
-        refresh_token_data: dict[str, Any] = {
-            "sub": user_id,
-            "exp": datetime.now() + timedelta(days=7)
-        }
-
         access_token: str = jwt.encode( # type: ignore
             access_token_data, os.getenv("SECRET_KEY"), algorithm="HS256"
         )
-        refresh_token: str = jwt.encode( # type: ignore
-            refresh_token_data, os.getenv("SECRET_KEY"), algorithm="HS256"
-        )
-        return access_token, refresh_token 
+        return access_token
 
     
     def current_user(self, request: Request | None=None) -> User | None:
@@ -100,8 +87,4 @@ class JWTBearerAuth(Auth):
             return
 
         user_obj = cast(User, get_obj("User", decoded_data["sub"]))
-        logger.debug(f"auth_header: {auth_header}")
-        logger.debug(f"base64_auth_header: {token}")
-        logger.debug(f"decoded_base64_auth: {decoded_data}")
-        logger.debug(f"user_obj: {user_obj}")
         return user_obj
