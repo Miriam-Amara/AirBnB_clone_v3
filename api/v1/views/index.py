3d9c49ffb5268a5d4e73325a6387325e51633e36
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-09-05 20:11:27 +0100

    refactor(api/v1/views/index): add strict_slashes to all routes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,11 +9,11 @@ from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route("/status")
+@app_views.route("/status", strict_slashes=False)
 def status() -> dict[str, str]:
     return {"status": "OK"}
 
-@app_views.route("/stats")
+@app_views.route("/stats", strict_slashes=False)
 def stats() -> dict[str, int]:
     """Returns the number of objects in storage"""
     return storage.count()
