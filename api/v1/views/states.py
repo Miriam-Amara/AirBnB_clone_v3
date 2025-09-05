6df567e8f9458a523ddae48f014efa24ce454888
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-09-05 01:05:18 +0100

    refactor(api/v1/views/states): use utils functions for object retrieval and request data
    Removed local get_obj() and JSON try/except in states.py. Now using
    get_obj() and get_request_data() from utils for consistency and code reuse.

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,10 +4,11 @@
 
 """
 
-from flask import request, abort, jsonify
+from flask import abort, jsonify
 import logging
 
 from api.v1.views import app_views
+from api.v1.views.utils import get_obj, get_request_data
 from models.state import State
 from models import storage
 
@@ -24,14 +25,6 @@ if disable_logging:
     logging.disable(logging.CRITICAL)
 
 
-def get_obj(state_id: str):
-    """Checks if object with the given id exists in database"""
-    obj = storage.get("State", state_id)
-    logging.debug(f"{obj}")
-    if not obj:
-        abort(404)
-    return obj
-
 @app_views.route("/states")
 def states():
     """Retrieves the list of objects of all states."""
@@ -41,10 +34,7 @@ def states():
 @app_views.route("/states", methods=["POST"])
 def create_state():
     """Creates a new state and save it to the database."""
-    try:
-        state_data = request.get_json()
-    except Exception:
-        abort(400, description="Not a json")
+    state_data = get_request_data()
 
     if "name" not in state_data:
         abort(400, description="Missing name")
@@ -61,13 +51,13 @@ def create_state():
 @app_views.route("/states/<state_id>")
 def get_state(state_id: str):
     """Returns a state object by id"""
-    state_obj = get_obj(state_id)
+    state_obj = get_obj("State", state_id)
     return state_obj.to_dict()
 
 @app_views.route("/states/<state_id>", methods=["DELETE"])
 def delete_state(state_id: str):
     """Deletes state object from database"""
-    state_obj = get_obj(state_id)
+    state_obj = get_obj("State", state_id)
     storage.delete(state_obj)
     storage.save()
     return jsonify({}), 200
@@ -75,12 +65,8 @@ def delete_state(state_id: str):
 @app_views.route("/states/<state_id>", methods=["PUT"])
 def update_state(state_id: str):
     """Updates a state in the database"""
-    state_obj = get_obj(state_id)
-
-    try:
-        state_data = request.get_json()
-    except Exception:
-        abort(400, description="Not a json")
+    state_obj = get_obj("State", state_id)
+    state_data = get_request_data()
     
     state_data.pop("id", None)
     state_data.pop("created_at", None)
